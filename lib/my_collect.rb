c0f3daf38eb2a24a19c1789421d914a28a6fb89b
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-07 21:23:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,7 @@ def my_collect(array)
   new array = []
   while index < array.length
         new_array << yield(array[index])
-      name.split(" ").first
-    index += 1
+        index += 1
   end
   new_array
 end
