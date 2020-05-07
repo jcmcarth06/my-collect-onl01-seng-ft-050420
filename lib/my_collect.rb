95a03cf58da978edcf75ceb8eb4c767983a55a43
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-07 21:20:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect(array)
   index = 0
   while index < array.length
-    my_collect_array do |name|
+    my_collect_array do |student|
       name.split(" ").first
     index += 1
   end
